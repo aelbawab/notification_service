45a2f852c4408680d1aabc96ce1e17c2dc059f2b
Abdelrhman Mohamed <aelbawab@arabiclocalizer.com>
2023-06-26 17:38:55 +0300

    init

---
+++ index.js
@@ -0,0 +1,88 @@
+const express = require('express');
+const { google } = require('googleapis');
+const { default: Axios } = require('axios');
+const dotenv = require('dotenv');
+
+
+dotenv.config();
+const app = express();
+const port = 2005;
+
+app.use(express.json());
+
+
+app.post('/', (req, res) => {
+    console.log('Received request:', req.body);
+    sendNotification(req);
+
+    res.send('Request received!');
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
+
+var MESSAGING_SCOPE = "https://www.googleapis.com/auth/firebase.messaging";
+var SCOPES = [MESSAGING_SCOPE];
+
+function getAccessToken() {
+    return new Promise(function (resolve, reject) {
+        const jwtClient = new google.auth.JWT(
+            process.env.client_email,
+            null,
+            process.env.private_key,
+            SCOPES,
+            null
+        );
+        jwtClient.authorize(function (err, tokens) {
+            if (err) {
+                reject(err);
+                return;
+            }
+            // console.log({ tokens: tokens.access_token })
+            console.log(tokens)
+            resolve(tokens);
+        });
+    });
+}
+
+let expiry_date = Date.now();
+let access_token = null;
+
+const sendNotification = async (req) => {
+
+    let data = JSON.stringify(req.body)
+    console.log(expiry_date, access_token)
+    try {
+        if (!access_token || expiry_date < Date.now()) {
+            const googleResponse = await getAccessToken();
+            access_token = googleResponse.access_token;
+            expiry_date = googleResponse.expiry_date - (300 * 1000);
+        }
+        console.log(expiry_date, access_token)
+
+        var config = {
+            method: 'post',
+            url: 'https://fcm.googleapis.com/v1/projects/attendance-84eba/messages:send',
+            headers: {
+                'Authorization': `Bearer ${access_token}`,
+                'Content-Type': 'application/json'
+            },
+            data: data
+        };
+
+        Axios(config)
+            .then(function (response) {
+                console.log(JSON.stringify(response.data));
+            })
+            .catch(function (error) {
+                console.log(error.data);
+            });
+
+    } catch (error) {
+
+    }
+
+
+}
+
